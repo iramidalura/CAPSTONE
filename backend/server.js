c7a6ecc372c98dc5091c01eaf8694f9fe1197e85
eramae21 <dalura.eramae@gmail.com>
2025-01-14 09:33:48 +0800

    backend

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server, { cors: { origin: 'http://localhost:5173' } });
+const io = socketIo(server, { cors: { origin: 'https://kiddiecare.netlify.app/' } });
 const PORT = process.env.PORT || 3000;
 
 // Middleware
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use(express.json({
-  origin: 'http://localhost:5173', // Allow only the frontend's origin
+  origin: 'https://kiddiecare.netlify.app/', // Allow only the frontend's origin
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 })); // Use built-in JSON body parser from Express
