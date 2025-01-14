3d572f920f3b8ffc49734157c5d1294fe6276c25
eramae21 <dalura.eramae@gmail.com>
2025-01-14 20:05:40 +0800

    initial commit

--- CAPSTONE-FINAL-main/vite.config.js
+++ CAPSTONE-FINAL-main/vite.config.js
@@ -1,31 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      buffer: 'buffer/',
-      stream: 'stream-browserify',
-      util: 'util',
+      buffer: 'buffer/',            // Polyfill for buffer
+      stream: 'stream-browserify',  // Polyfill for stream
+      util: 'util',                 // Polyfill for util
+      // Add other modules as needed
     },
   },
   define: {
-    global: 'window',
+    global: 'window', // Sets global variable to window for browser compatibility
   },
   build: {
     rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id
-              .toString()
-              .split('node_modules/')[1]
-              .split('/')[0]
-              .toString();
-          }
-        },
-      },
+      external: ['util'], // Exclude util if it is not directly needed
     },
   },
 });
