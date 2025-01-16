9f6e2165965a07cf0ef7a73ebf418685bc564bfb
eramae21 <dalura.eramae@gmail.com>
2025-01-17 04:39:51 +0800

    ini

--- CAPSTONE-FINAL-main/src/pages/Pediatrician/API.jsx
+++ CAPSTONE-FINAL-main/src/pages/Pediatrician/API.jsx
@@ -1,5 +1,5 @@
 //This is the Auth token, you will use it to generate a meeting and connect to it
-export const authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiI1YWFlYTEzYS02ZmJmLTRhM2QtOGZlZC1lZjNmY2ZhOTgyYTAiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIiwiYWxsb3dfY3JlYXRlX3Jvb20iXSwiaWF0IjoxNzM3MDU4OTQ1LCJleHAiOjE3MzcxNDUzNDV9.XFgtG45eW5fp6sU-j8RWWds4-1oXm-SvEHQrQS3eNXo";
+export const authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiI1YWFlYTEzYS02ZmJmLTRhM2QtOGZlZC1lZjNmY2ZhOTgyYTAiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIl0sImlhdCI6MTczNzA1OTg3OCwiZXhwIjoxNzQ0ODM1ODc4fQ.dhIKbImz9bTTmi_qTfhiur9Yo4EUNPHakxQwUAOj1Mg";
 // API call to create a meeting
 export const createMeeting = async ({ token }) => {
   const res = await fetch(`https://api.videosdk.live/v2/rooms`, {
