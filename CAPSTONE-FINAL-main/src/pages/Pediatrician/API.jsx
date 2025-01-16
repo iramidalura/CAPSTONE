ddcd9b33f18d819653aae933e5be24d93a1bbb21
eramae21 <dalura.eramae@gmail.com>
2025-01-17 04:07:48 +0800

    initial commit

--- CAPSTONE-FINAL-main/src/pages/Pediatrician/API.jsx
+++ CAPSTONE-FINAL-main/src/pages/Pediatrician/API.jsx
@@ -1,11 +1,11 @@
 //This is the Auth token, you will use it to generate a meeting and connect to it
-export const authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJqYmFjZW5hczBAZ21haWwuY29tIiwidXNlclR5cGUiOiJQZWRpYXRyaWNpYW4iLCJpYXQiOjE3MzcwNTY4MzQsImV4cCI6MTczNzA2MDQzNH0.bnS7VzxbhxhnFcxSsXZTybuCmJZzJ-naSPTf0FfLD18";
+export const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJqYmFjZW5hczBAZ21haWwuY29tIiwidXNlclR5cGUiOiJQZWRpYXRyaWNpYW4iLCJpYXQiOjE3MzcwNTY4MzQsImV4cCI6MTczNzA2MDQzNH0.bnS7VzxbhxhnFcxSsXZTybuCmJZzJ-naSPTf0FfLD18";
 // API call to create a meeting
 export const createMeeting = async ({ token }) => {
   const res = await fetch(`https://api.videosdk.live/v2/rooms`, {
     method: "POST",
     headers: {
-      authorization: `${authToken}`,
+      Authorization: `Bearer ${token}`,
       "Content-Type": "application/json",
     },
     body: JSON.stringify({}),
